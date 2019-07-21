3934459f1218540a81824f4ed9cb4db66b9722d2
Carattiniw <45584185+Suddenrebel@users.noreply.github.com>
2019-07-21 18:58:00 -0400

    Tutorial 3 Complete

---
+++ Space Shooter/Assets/Scripts/destroyByContact.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private gameController gameController;
+
+
+    void Start () //will find our gameController script
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent <gameController>();
+        }
+
+        if (gameController == null)
+        {
+            Debug.Log ("Cannot find 'GameController' script");
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Boundary") //will ignore the boundary gameObject
+        {
+            return;
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+
+        gameController.AddScore (scoreValue);
+        Destroy(other.gameObject); //destroy any object shot by player
+        Destroy(gameObject); //destroy player if make contact with asteroid or enemy
+    }
+}
---
+++ Space Shooter/Assets/Scripts/gameController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class gameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI restartText;
+    public TextMeshProUGUI gameoverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    void Start ()
+    {
+        gameOver = false;
+        restart = false;
+        scoreText.text = "";
+        gameoverText.text = "";
+        restartText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves ());
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))//restart game with r key
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+
+        if (Input.GetKey("escape"))//exit game with escape key
+        {
+            Application.Quit();
+        }
+    }
+
+    IEnumerator SpawnWaves ()
+    {
+        yield return new WaitForSeconds (startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate (hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds (spawnWait);
+            }
+            yield return new WaitForSeconds (waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart or 'Esc' to quit";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore (int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameoverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
